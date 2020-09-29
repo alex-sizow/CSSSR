9330a96b68a511a5a2b3ef240b141f3b3e3e3846
Alex <alex_sizow@outlook.com>
2020-09-29 14:31:21 +0300

    React component

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div class="header">Список товаров</div>
+      <ul class="list">
+        <li>Товар 1</li>
+        <li>Товар 2</li>
+        <li>Товар 3</li>
+        <li>Товар 4</li>
+        <li>Товар 5</li>
+        <li>Товар 6</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
